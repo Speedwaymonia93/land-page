739bcb072c00a77f66e1f0a1a8f8c9fb35164d9e
Monika <monikastrzalka@gmail.com>
2024-08-05 18:23:44 +0200

    Test commit

--- app/components/News.tsx
+++ app/components/News.tsx
@@ -9,7 +9,7 @@ const News = () => {
   return (
      <section className="max-container padding-container flex flex-col gap-20 py-10 pb-32 md:gap-28 lg:py-20 xl:flex-row">
       <div className="relative z-20 flex flex-1 flex-col xl:w-1/2">
-        <h1 className="text-4xl text-center font-bold">{language === 'pl' ? PAGE_TITLE_PL : PAGE_TITLE_DE}</h1>
+        <h1 className="text-4xl text-center font-bold">{language === 'pl' ? "Witamy na stronie Krajowego Związku Towarzystw Polsko-Niemieckich!" : "Willkommen auf der Website des Landesverbandes der Polnisch-Deutschen Gesellschaften!"}</h1>
       </div>
     </section>
   )
