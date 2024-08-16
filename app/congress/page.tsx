bf8d3bc2d80d22b0c982ae83b9409b531ce0789e
Monika <monikastrzalka@gmail.com>
2024-08-16 13:35:28 +0200

    Replacement of programme files and new info addition.

--- app/congress/page.tsx
+++ app/congress/page.tsx
@@ -2,13 +2,13 @@
 import React from "react";
 import FormComponent from "../components/FormComponent";
 import { useLanguage } from '../context/LanguageContext';
-import { ADDRESS_DE, ADDRESS_PL, PRICE_DE, PRICE_PL, ATTENTION_DE, ATTENTION_PL, CONGRESS_TITLE_PL, CONGRESS_TITLE_DE} from '../constants/index';
+import { ADDRESS_DE, ADDRESS_PL, PRICE_DE, PRICE_PL, ATTENTION_DE, ATTENTION_PL, CONGRESS_TITLE_PL, CONGRESS_TITLE_DE, CONGRESS_DESCRIPTION_PL, CONGRESS_DESCRIPTION_DE} from '../constants/index';
 const Congress: React.FC = () => {
   const { language } = useLanguage();
   return <div className="flex justify-center flex-col">
     <h2 className='text-4xl text-center pb-2'>{language === 'pl' ? CONGRESS_TITLE_PL : CONGRESS_TITLE_DE}</h2>
     <div></div>
-    <a className="text-center mb-8" href={language === 'pl' ? '/Program_ramowy_Kongress_Bielsko_Biala_11.3.2024.pdf' : '/Rahmenprogramm_Kongress_Bielsko-Biala.pdf'} target="_blank" rel='noopener noreferrer'>
+    <a className="text-center mb-8" href={language === 'pl' ? '/Ramowy program_Kongres_Bielsko-Biala.pdf' : '/Rahmenprogramm_Kongress_Bielsko-Biala.pdf'} target="_blank" rel='noopener noreferrer'>
         <button className='mt-6 p-4 rounded-lg bg-rose-700 hover:bg-rose-400 active:bg-rose-400 focus:outline-none focus:ring focus:ring-rose-700 hover:text-white'>
           {language === 'pl' ? 'Program ramowy ' : 'Rahmenprogramm'}
         </button>
@@ -18,7 +18,8 @@ const Congress: React.FC = () => {
     <div className="text-left w-1/2 mx-auto">
 <p className="mb-2"> {language === 'pl' ? ADDRESS_PL : ADDRESS_DE}</p>
     <p className="mb-2"> {language === 'pl' ? PRICE_PL : PRICE_DE}</p>
-    <p className="mb-2"> {language === 'pl' ? ATTENTION_PL : ATTENTION_DE}</p>
+      <p className="mb-2"> {language === 'pl' ? ATTENTION_PL : ATTENTION_DE}</p>
+      <div className="mb-2">{language === 'pl' ? CONGRESS_DESCRIPTION_PL.map((paragraph, index) => <p className="mb-4" key={index}>{ paragraph}</p>) : CONGRESS_DESCRIPTION_DE.map((paragraph, index) => <p className="mb-4" key={index}>{ paragraph}</p>)}</div>
     </div>
     
     <FormComponent/>
